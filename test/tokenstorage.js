8e2e6580071b57a166bed383b8d8f9ec6e13dc74
Gísli Kristjánsson <gislik@hamstur.is>
2017-06-22 17:06:35 +0000

    Logic error in test fixed

--- test/tokenstorage.js
+++ test/tokenstorage.js
@@ -1,19 +1,18 @@
 var TokenStorage = artifacts.require("./TokenStorage.sol");
 
-contract("TokenStorage", () => {
+contract("TokenStorage", (accounts) => {
   it("should not have initial supply", () => {
     return TokenStorage.deployed().then((db) => {
-      return db.totalSupply();
+      return db.getSupply();
     }).then((supply) => {
       assert.equal(supply.valueOf(), 0, "total supply is not 0");
     });
   });
-  it("should have a supply of 10000 after explicitly setting to 10000", (accounts) => {
+  it("should have a supply of 10000 after explicitly setting to 10000", () => {
     return TokenStorage.deployed().then((db) => {
-      db.addBalance(accounts[0], 10000).then(() => {
-        return db.totalSupply();
-      }).then((supply) => {
-        assert.equal(supply.valueOf(), 1000, "total supply is not 10000");
+      db.addBalance(accounts[0], 0);
+      db.getSupply().then((supply) => {
+        assert.equal(supply.valueOf(), 10000, "total supply is not 10000");
       });
     });
   });
