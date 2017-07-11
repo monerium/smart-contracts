d1eb4db26e40533d213468019aead746c2ad0ff2
Gísli Kristjánsson <gislik@hamstur.is>
2017-07-11 15:58:32 +0000

    Configuration for monerium devnet

--- truffle.js
+++ truffle.js
@@ -1,9 +1,23 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
-};
+	networks: {
+		development: {
+			host: "localhost",
+			port: 8545,
+			network_id: "*" // Match any network id
+		},
+		monerium: {
+			host: "e.monerium.com", // Random IP for example purposes (do not use)
+			port: 8549,
+			network_id: 2000,        // monerium testnet
+			// from: "0x7c22a71d7db4601b1675ea81a93c67940319bdfc",
+			from: "0x253c61c9e3d1aa594761f7ef3f7cbe7a5151f9fd"
+			// gas: 3000000,
+			// optional config values:
+			// gas
+			// gasPrice
+			// from - default address to use for any transaction Truffle makes during migrations
+			// provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+			//          - if specified, host and port are ignored.
+		},
+  },
+}
