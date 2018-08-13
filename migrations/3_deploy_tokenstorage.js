d089241acbc56c9e56ec40814f046a40540f7d4d
Gísli Kristjánsson <gislik@hamstur.is>
2018-08-13 14:31:03 +0000

    Cleaning up deployment scripts

--- migrations/3_deploy_tokenstorage.js
+++ migrations/3_deploy_tokenstorage.js
@@ -1,35 +1,10 @@
 // artifacts
-var SafeMathLib = artifacts.require("./SafeMathLib.sol");
-var ERC20Lib = artifacts.require("./ERC20Lib.sol");
-var MintableTokenLib = artifacts.require("./MintableTokenLib.sol");
-var SmartTokenLib = artifacts.require("./SmartTokenLib.sol");
-var BlacklistValidator = artifacts.require("./BlacklistValidator.sol");
-var ConstantValidator = artifacts.require("./ConstantValidator.sol");
-
 var TokenStorageLib = artifacts.require("./TokenStorageLib.sol");
 var TokenStorage = artifacts.require("./TokenStorage.sol");
-var StandardController = artifacts.require("./StandardController.sol");
-var MintableTokenLib = artifacts.require("./MintableTokenLib.sol");
-var MintableController = artifacts.require("./MintableController.sol");
-var SmartController = artifacts.require("./SmartController.sol");
-var ConstantSmartController = artifacts.require("./ConstantSmartController.sol");
-var USD = artifacts.require("./USD.sol");
-var EUR = artifacts.require("./EUR.sol");
-
-// TODO: Registry?
 
 module.exports = function(deployer) {
 
-  // deploy and link controllers
-  // deployer.link(TokenStorageLib, ConstantSmartController);
-  // deployer.link(ERC20Lib, ConstantSmartController);
-  // deployer.link(MintableTokenLib, ConstantSmartController);
-  // deployer.link(SmartTokenLib, ConstantSmartController);
-
-  // deployer.deploy([[TokenStorage, 10000], [StandardController, 0x0, 50000]]);
-  // deployer.link(MintableTokenLib, [MintableController, SmartController]);
-  // deployer.deploy(MintableController, 0x0, 0x0, 0);
-
   deployer.link(TokenStorageLib, TokenStorage);
   deployer.deploy(TokenStorage, 10000);
+
 };
--- migrations/6_deploy_validators.js
+++ migrations/6_deploy_validators.js
@@ -1,22 +1,5 @@
 // artifacts
-var SafeMathLib = artifacts.require("./SafeMathLib.sol");
-var ERC20Lib = artifacts.require("./ERC20Lib.sol");
-var MintableTokenLib = artifacts.require("./MintableTokenLib.sol");
-var SmartTokenLib = artifacts.require("./SmartTokenLib.sol");
 var BlacklistValidator = artifacts.require("./BlacklistValidator.sol");
-var ConstantValidator = artifacts.require("./ConstantValidator.sol");
-
-var TokenStorageLib = artifacts.require("./TokenStorageLib.sol");
-var TokenStorage = artifacts.require("./TokenStorage.sol");
-var StandardController = artifacts.require("./StandardController.sol");
-var MintableTokenLib = artifacts.require("./MintableTokenLib.sol");
-var MintableController = artifacts.require("./MintableController.sol");
-var SmartController = artifacts.require("./SmartController.sol");
-var ConstantSmartController = artifacts.require("./ConstantSmartController.sol");
-var USD = artifacts.require("./USD.sol");
-var EUR = artifacts.require("./EUR.sol");
-
-// TODO: Registry?
 
 module.exports = function(deployer) {
 
