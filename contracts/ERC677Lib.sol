465e6569015eaa40974fad3b21c48e1f9ac95381
Gísli Kristjánsson <gislik@hamstur.is>
2018-08-24 23:29:58 +0000

    Documentation for ERC677Lib #12

--- contracts/ERC677Lib.sol
+++ contracts/ERC677Lib.sol
@@ -5,28 +5,48 @@ import "./ITokenRecipient.sol";
 import "./TokenStorage.sol";
 import "./ERC20Lib.sol";
 
+/**
+ * @title ERC677
+ * @dev ERC677 token functionality.
+ * https://github.com/ethereum/EIPs/issues/677
+ */
 library ERC677Lib {
 
     using ERC20Lib for TokenStorage;
     using AddressUtils for address;
 
-    // EVENTS
+    /**
+     * @dev Emitted when tokens are transferred.
+     * @param from Sender address.
+     * @param to Recipient address.
+     * @param amount Number of tokens transferred.
+     * @param data Additional data passed to the recipient's tokenFallback method.
+     */
     event Transfer(address indexed from, address indexed to, uint amount, bytes data);
 
+    /**
+     * @dev Transfers tokens and subsequently calls a method on the recipient [ERC677].
+     * If the recipient is a non-contract address this method behaves just like transfer.
+     * @param db Token storage to operate on.
+     * @param caller Address of the caller passed through the frontend.
+     * @param to Recipient address.
+     * @param amount Number of tokens to transfer.
+     * @param data Additional data passed to the recipient's tokenFallback method.
+     */
     function transferAndCall(
         TokenStorage db, 
         address caller, 
-        address receiver, 
+        address to, 
         uint256 amount, 
         bytes data
     ) 
         external
         returns (bool) 
     {
-        if (db.transfer(caller, receiver, amount)) {
-            emit Transfer(caller, receiver, amount, data);
-            if (receiver.isContract()) {
-                ITokenRecipient recipient = ITokenRecipient(receiver);
+        if (db.transfer(caller, to, amount)) {
+            emit Transfer(caller, to, amount, data);
+            if (to.isContract()) {
+                ITokenRecipient recipient = ITokenRecipient(to);
                 recipient.tokenFallback(caller, amount, data);
             }
             return true;
