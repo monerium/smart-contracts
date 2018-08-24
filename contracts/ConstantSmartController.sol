3e06f1ae0bfe563d2de0a73787786ee4a2563d06
Gísli Kristjánsson <gislik@hamstur.is>
2018-08-24 21:35:30 +0000

    Documentation for ConstantSmartController #12

--- contracts/ConstantSmartController.sol
+++ contracts/ConstantSmartController.sol
@@ -3,9 +3,17 @@ pragma solidity ^0.4.24;
 import "./ConstantValidator.sol";
 import "./SmartController.sol";
 
+/**
+ * @title ConstantSmartController
+ * @dev Constantly rejects token transfers by using a rejecting validator.
+ */
 contract ConstantSmartController is SmartController {
 
-    // CONSTRUCTOR
+    /**
+     * @dev Contract constructor.
+     * @param storage_ Address of the new storage.
+     * @param ticker 3 letter currency ticker.
+     */
     constructor(address storage_, bytes3 ticker) 
         public
         SmartController(storage_, new ConstantValidator(false), ticker)
