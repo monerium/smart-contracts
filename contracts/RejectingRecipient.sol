9a68c2a24de21a20fd6c9ae4bb530d5a9df3e1cf
Gísli Kristjánsson <gislik@hamstur.is>
2018-08-24 23:46:14 +0000

    Documentation for RejectingRecipient #12

--- contracts/RejectingRecipient.sol
+++ contracts/RejectingRecipient.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.24;
 
 import "./ITokenRecipient.sol";
 
+/**
+ * @title RejectingRecipient
+ * @dev [ERC677]-compatible contract.
+ * The contract rejects token ownership.
+ */
 contract RejectingRecipient is ITokenRecipient {
 
     function tokenFallback(address, uint256, bytes) external returns (bool) {
