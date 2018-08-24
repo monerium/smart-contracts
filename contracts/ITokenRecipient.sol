9c7b1a238fd0767c46d3abf465ff76f078c1ea61
Gísli Kristjánsson <gislik@hamstur.is>
2018-08-24 23:40:53 +0000

    Documentation for ITokenRecipient #12

--- contracts/ITokenRecipient.sol
+++ contracts/ITokenRecipient.sol
@@ -1,7 +1,17 @@
 pragma solidity ^0.4.24;
 
+/**
+ * @title ITokenRecipient
+ * @dev Contracts implementing this interface can participate in [ERC223 and ERC677].
+ */
 interface ITokenRecipient { 
 
+    /**
+     * @dev Receives notification from [ERC223 and ERC677] transferAndCall.
+     * @param from Sender address.
+     * @param amount Number of tokens.
+     * @param data Additional data.
+     */
     function tokenFallback(address from, uint256 amount, bytes data) external returns (bool); 
 
 }
