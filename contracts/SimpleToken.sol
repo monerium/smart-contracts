59a9feac831e793ea7c7a0f961d965561746dc66
Gísli Kristjánsson <gislik@hamstur.is>
2018-08-25 14:13:08 +0000

    Documentation for SimpleToken #12

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.24;
 
 import "zeppelin-solidity/contracts/token/ERC20/BasicToken.sol";
 
+/**
+ * @title SimpleToken
+ * @dev This simple [ERC20] compatible token is used in the test suite.
+ */
 contract SimpleToken is BasicToken {
 
     constructor() public {
