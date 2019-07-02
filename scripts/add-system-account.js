91224add991727aea802eb2f4539efc2b104283a
Gísli Kristjánsson <gislik@hamstur.is>
2019-07-02 17:30:46 +0000

    script updated to take the address to the smart controller as an argument

--- scripts/add-system-account.js
+++ scripts/add-system-account.js
@@ -2,17 +2,18 @@ var SmartController = artifacts.require("./SmartController.sol");
 
 module.exports = async function(exit) {
 
-  if (process.argv.length < 5) {
-    console.log(`Usage: ${process.argv.join(" ")} <account>`)
+  if (process.argv.length < 6) {
+    console.log(`Usage: ${process.argv.join(" ")} <smart-controller> <account>`)
     exit(1);
   }
 
   const len = process.argv.length;
+  const address = process.argv[len-2];
   const account = process.argv[len-1];
   console.log(`adding ${account}`);
 
   try {
-    const controller = await SmartController.deployed();
+    const controller = await SmartController.at(address);
     const tx = await controller.addSystemAccount(account);
     console.log(tx);
     exit(0);
