e5d176978deef4b439a2b214b597afb3c80e4c77
Gísli Kristjánsson <gislik@hamstur.is>
2019-07-19 01:27:31 +0000

    [fix] linking TokenStorageLib to SmartController

--- migrations/8_deploy_usd.js
+++ migrations/8_deploy_usd.js
@@ -2,6 +2,7 @@ var BlacklistValidator = artifacts.require("./BlacklistValidator.sol");
 var SmartController = artifacts.require("./SmartController.sol");
 var USD = artifacts.require("./USD.sol");
 
+var TokenStorageLib = artifacts.require("./TokenStorageLib.sol");
 var MintableTokenLib = artifacts.require("./MintableTokenLib.sol");
 var SmartTokenLib = artifacts.require("./SmartTokenLib.sol");
 var ERC20Lib = artifacts.require("./ERC20Lib.sol");
@@ -9,10 +10,11 @@ var ERC677Lib = artifacts.require("./ERC677Lib.sol");
 
 module.exports = function(deployer) {
 
+  deployer.link(TokenStorageLib, SmartController);
+  deployer.link(MintableTokenLib, SmartController);
   deployer.link(SmartTokenLib, SmartController);
   deployer.link(ERC20Lib, SmartController);
   deployer.link(ERC677Lib, SmartController);
-  deployer.link(MintableTokenLib, SmartController);
 
   return deployer.deploy(USD).then(frontend => 
     deployer.deploy(SmartController, 0x0, BlacklistValidator.address, "USD", frontend.address).then(controller => 
