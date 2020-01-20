12bfc8eb0f49b0ab4391f04af4c95e4be868abac
Gísli Kristjánsson <gislik@hamstur.is>
2020-01-20 15:24:59 +0000

    Updating scripts

--- scripts/remove-system-account.js
+++ scripts/remove-system-account.js
@@ -1,20 +1,24 @@
 var SmartController = artifacts.require("./SmartController.sol");
+var TokenFrontend = artifacts.require("./TokenFrontend.sol");
 
 module.exports = async function(exit) {
 
   if (process.argv.length < 6) {
-    console.log(`Usage: ${process.argv.join(" ")} <smart-controller> <account>`)
+    console.log(`Usage: ${process.argv.join(" ")} <token> <account>`)
     exit(1);
   }
 
   const len = process.argv.length;
-  const controller = process.argv[len-2];
+  const address = process.argv[len-2];
   const account = process.argv[len-1];
   console.log(`removing ${account}`);
 
   try {
-    const c = await SmartController.at(controller);
-    const tx = await c.removeSystemAccount(account);
+    const token = await TokenFrontend.at(address);
+    const x = await token.getController();
+    console.log(`token: ${address}, controller: ${x}`);
+    const controller = await SmartController.at(x);
+    const tx = await controller.removeSystemAccount(account);
     console.log(tx);
     exit(0);
   } catch (e) {
--- scripts/transfer-token-ownership.js
+++ scripts/transfer-token-ownership.js
@@ -4,22 +4,24 @@ var TokenFrontend = artifacts.require("./TokenFrontend.sol");
 module.exports = async function(exit) {
 
   if (process.argv.length < 6) {
-    console.log(`Usage: ${process.argv.join(" ")} <token> <account>`)
+    console.log(`Usage: ${process.argv.join(" ")} <token> <new owner>`)
     exit(1);
   }
 
   const len = process.argv.length;
   const address = process.argv[len-2];
   const account = process.argv[len-1];
-  console.log(`adding ${account}`);
+  console.log(`token ${address} to ${account}`);
 
   try {
     const token = await TokenFrontend.at(address);
+    const tx1 = await token.transferOwnership(account);
+    console.log(tx1);
     const x = await token.getController();
-    console.log(`token: ${address}, controller: ${x}`);
-    const controller = await SmartController.at(x);
-    const tx = await controller.addSystemAccount(account);
-    console.log(tx);
+    console.log(`controller ${x} to ${account}`);
+    const controller = SmartController.at(x);
+    const tx2 = await controller.transferOwnership(account);
+    console.log(tx2);
     exit(0);
   } catch (e) {
     exit(e);
