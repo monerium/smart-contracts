9e4ce7c46a88c74ee536ca9ed871685e7b4da197
Kristen <kristenpire@hotmail.fr>
2022-03-09 18:26:36 +0100

    feat(test/ownership): addition of test on depricated smart-contract copied from openzepplin-solidty. The tests itself come from the openzepplin-solidity repository and homemade developpment

---
+++ contracts/ownership/mocks/ForceEther.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// @title Force Ether into a contract.
+// @notice  even
+// if the contract is not payable.
+// @notice To use, construct the contract with the target as argument.
+// @author Remco Bloemen <remco@neufund.org>
+contract ForceEther {
+
+  constructor() payable { }
+
+  function destroyAndSend(address payable _recipient) public {
+    selfdestruct(_recipient);
+  }
+}
