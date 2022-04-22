781990c5d3a9cf047d739336e1fe39c6258168ac
Gísli Kristjánsson <gislik@hamstur.is>
2022-04-22 15:22:33 +0000

    chore: updating copyright to 2022

--- contracts/BlacklistValidator.sol
+++ contracts/BlacklistValidator.sol
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: apache-2.0 */
 /**
- * Copyright 2019 Monerium ehf.
+ * Copyright 2022 Monerium ehf.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
