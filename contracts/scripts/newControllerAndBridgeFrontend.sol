7cfc852c2b6280560dba4d31dc7dc7689a06856c
Kristen Pire <kristen@monerium.com>
2023-07-10 16:00:04 +0200

    feat(testContract): Creation of a new tests directory in contracts

--- contracts/scripts/newControllerAndBridgeFrontend.sol
+++ contracts/scripts/newControllerAndBridgeFrontend.sol
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: apache-2.0 */
 pragma solidity 0.8.11;
 
-import "./SmartController.sol";
-import "./EUR.sol";
+import "../SmartController.sol";
+import "../EUR.sol";
 
 contract NewControllerAndBridgeFrontend {
     SmartController internal controller;
