aa5b15d70763ba0cc93f377165662fef16278673
Kristen Pire <kristen@monerium.com>
2023-11-01 12:18:39 +0100

    fix(test): fix recovery Id before testing BurnFrom

--- test/5-usd.js
+++ test/5-usd.js
@@ -23,18 +23,22 @@ const AddressZero = "0x0000000000000000000000000000000000000000";
 const hash = `0xa1de988600a42c4b4ab089b619297c17d53cffae5d5120d82d8a92d0bb3b78f2`;
 const wallets = {
   "trust wallet": {
+    name: "trust wallet",
     address: `0xB0A6Ed7Fa5C6C5cc507840924591C1494eF47D04`,
     signature: `0xb5354ef622856f2acd9926752828d609b74471fa349891c70ec4512da5b7b8695418c39d82057dc09c480e8e65c5362327e882033e670e24b6a701983d93e18e1c`,
   },
   "ledger s nano": {
+    name: "ledger s nano",
     address: `0x4A6bBDa876699420965147c26C3F3DF0bDc8eCab`,
     signature: `0xbad1a0d53be0e56724d877e053a199ca732c5e6e40d129c5a0980aa7d2c6582166af81969b23004ae81694e9518dcf39eb3af71d2847409376151f03465a0bf600`,
   },
   "meta mask": {
+    name: "meta mask",
     address: `0xFB7ce0578B4dc16803A3CB04fA0b286fCFfFF76d`,
     signature: `0x1e1769b8ca9ca3d7d4b747f15336c185aac391c89cd9b9bfaf26f0a38631690e64ccd925382278c055c527a58ffab853a9734d889a6bae2b920544645f8ce4361c`,
   },
   trezor: {
+    name: "trezor",
     address: `0xbc89Bea7B6156be4514f5D429e30240F4C2600e4`,
     signature: `0x884d210ebc1437a56e4507c47a5fadcf1076ef366d37d23937a515e8af63075605fbb6be98a9c9dd41bfc68b57795c79375ef10e34a8bf90701a2200ff5cc59d1b`,
   },
@@ -228,19 +232,18 @@ contract("USD", (accounts) => {
     it(`should burn 18 tokens from a non-system address [${name}]`, async () => {
       await usd.mintTo(wallet.address, 18, { from: system });
       const balance0 = await usd.balanceOf(wallet.address);
-      /*
+
       const sig = wallet.signature.replace(/^0x/, "");
       const r = `0x${sig.slice(0, 64)}`;
       const s = `0x${sig.slice(64, 128)}`;
       var v = web3.utils.hexToNumber(`0x${sig.slice(128, 130)}`);
-
       if (v < 27) v += 27;
       assert(v == 27 || v == 28);
 
-      await usd.burnFrom(wallet.address, 18, hash, v, r, s, { from: system });
+      //      await usd.burnFrom(wallet.address, 18, hash, v, r, s, { from: system });
+      const rebuiltSig = `0x${r.slice(2)}${s.slice(2)}${v.toString(16)}`;
 
-*/
-      await controller.burnFrom(wallet.address, 18, hash, wallet.signature, {
+      await controller.burnFrom(wallet.address, 18, hash, rebuiltSig, {
         from: system,
       });
       const balance1 = await usd.balanceOf(wallet.address);
