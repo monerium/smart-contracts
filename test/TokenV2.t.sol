2ecedaf4319a8808b7b4ad091f1795bc39ff954a
Kristen Pire <kristen.pire@epitech.eu>
2024-08-28 10:57:56 +0200

    feat(batchApprove): adding a batchApprove in tokenV2 with script and tests

--- test/TokenV2.t.sol
+++ test/TokenV2.t.sol
@@ -8,7 +8,7 @@ import "../src/BlacklistValidatorUpgradeable.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract TokenTest is Test {
-    Token public token;
+    TokenV2 public tokenV2;
     ERC1967Proxy public proxy;
 
     function setUp() public {
@@ -32,31 +32,63 @@ contract TokenTest is Test {
         proxy = new ERC1967Proxy(address(implementation), initData);
 
         // Cast the proxy address to the Token interface
-        token = Token(address(proxy));
+        tokenV2 = TokenV2(address(proxy));
     }
 
     function testUpgradeToV2() public {
         // Verify initial state
-        assertEq(token.name(), "token");
-        assertEq(token.symbol(), "EURE");
+        assertEq(tokenV2.name(), "token");
+        assertEq(tokenV2.symbol(), "EURE");
 
         // Deploy the new TokenV2 implementation contract
         TokenV2 newImplementation = new TokenV2();
 
-        // Encode the initializeV2 call
-        bytes memory data = abi.encodeWithSelector(
-            TokenV2.initializeV2.selector
-        );
-
-        // Upgrade the proxy to use the new implementation contract and call initializeV2
-        token.upgradeToAndCall(address(newImplementation), data);
+        bytes memory data = "";
+        // Upgrade the proxy to use the new implementation contract
+        tokenV2.upgradeToAndCall(address(newImplementation), data);
 
         // Cast the proxy address to the TokenV2 interface
         TokenV2 upgradedToken = TokenV2(address(proxy));
 
         // Verify the upgrade was successful
-        assertEq(token.allowance(address(66), address(99)), 12e18);
         assertEq(upgradedToken.name(), "token");
         assertEq(upgradedToken.symbol(), "EURE");
+    }
+
+    function testBatchApprove() public {
+        // Deploy the new TokenV2 implementation contract
+        TokenV2 newImplementation = new TokenV2();
+
+        // Upgrade the proxy to use the new implementation contract
+        bytes memory data = "";
+        tokenV2.upgradeToAndCall(address(newImplementation), data);
+
+        // Cast the proxy address to the TokenV2 interface
+        TokenV2 upgradedToken = TokenV2(address(proxy));
+
+        // Create dummy addresses and values for testing
+        address[] memory from = new address[](3);
+        address[] memory to = new address[](3);
+        uint256[] memory values = new uint256[](3);
+
+        from[0] = address(0x123);
+        from[1] = address(0x456);
+        from[2] = address(0x789);
+
+        to[0] = address(0xabc);
+        to[1] = address(0xdef);
+        to[2] = address(0xded);
+
+        values[0] = 100;
+        values[1] = 200;
+        values[2] = 300;
+
+        // Call batchApprove function
+        upgradedToken.batchApprove(from, to, values);
+
+        // Verify the approvals
+        assertEq(upgradedToken.allowance(from[0], to[0]), values[0]);
+        assertEq(upgradedToken.allowance(from[1], to[1]), values[1]);
+        assertEq(upgradedToken.allowance(from[2], to[2]), values[2]);
     }
 }
