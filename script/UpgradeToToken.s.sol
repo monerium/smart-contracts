73d55ea2272715246d9e6d25d976947011fc55d5
Kristen Pire <kristen.pire@epitech.eu>
2024-08-29 14:22:59 +0200

    feat(GnosisUpgrade): configuring for gnosis V2_1_0 allowance migration

---
+++ script/UpgradeToToken.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../src/GnosisControllerToken.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract UpgradeToToken is Script {
+    function run() external {
+        // Load the deployer private key from the environment or specify it directly
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address proxyAddress = vm.envAddress("PROXY");
+
+        // Start the broadcast using the deployer's private key
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the new TokenV2 implementation contract
+        GnosisControllerToken newTokenV2Implementation = new GnosisControllerToken();
+
+        // Upgrade the proxy to use the new implementation contract
+        UUPSUpgradeable proxy = UUPSUpgradeable(proxyAddress);
+
+        bytes memory data = "";
+        // Upgrade the proxy to use the new implementation contract and call initializeV2
+        proxy.upgradeToAndCall(address(newTokenV2Implementation), data);
+
+        // End the broadcast
+        vm.stopBroadcast();
+    }
+}
